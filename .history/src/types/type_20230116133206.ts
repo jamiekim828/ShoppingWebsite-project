2e9334aed08366687b0a5873901a334591d8be11
mayuri2018 <mayuri.vijaya@gmail.com>
2023-01-16 15:06:23 +0100

    nothing

---
+++ .history/src/types/type_20230116133206.ts
@@ -0,0 +1,21 @@
+export type Country = {
+    flags: {
+      svg: string;
+    };
+    name: {
+      common: string;
+    };
+    region: string;
+    subregion : string;
+    population: number;
+    languages: object;
+    favorite: boolean;
+    capital: string[];
+    timezones:string [];
+    capitalInfo:{
+      latlng:string []
+    };
+    maps: {
+      googleMaps: string;
+    };
+  };
