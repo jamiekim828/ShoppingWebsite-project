f9fa0c270dfb7b8b8e608114cba19516d6a7970b
mayuri2018 <mayuri.vijaya@gmail.com>
2023-01-16 15:06:23 +0100

    nothing

--- .history/src/redux/slice/product_20230116132204.ts
+++ .history/src/redux/slice/product_20230116132204.ts
@@ -1,3 +1,1 @@
 import { createSlice } from '@reduxjs/toolkit';
-
-    
---
+++ .history/src/redux/slice/product_20230116140104.ts
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+type InitialType = {
+    countries: Country[];
+    favorite: Country[];
+  };
+  
+  const initialState: InitialType = {
+    countries: [],
+    favorite: [],
+  };
+  
+  const countrySlice = createSlice({
+    name: "country",
+    initialState,
+    reducers: {
+      getCountryData: (state, acion) => {
+        state.countries = acion.payload;
+      }
+    });
+
+export const productActions = productSlice.actions;
+export default productSlice.reducer;
+    
