7ebb796d315b96ce07e8ac9c5b06f9fdb266d5f9
jamiekim828 <jamiekim828@gmail.com>
2023-01-16 17:53:21 +0100

    Home upper part

---
+++ src/components/Home/HomeMidpart.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function HomeMidpart() {
+  return <div>HomeMidpart</div>;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material';
 
 import './index.css';
 import App from './App';
@@ -11,12 +11,21 @@ import store from './redux/store';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Montserrat',
+  },
+});
+
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
