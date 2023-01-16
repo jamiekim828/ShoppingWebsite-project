69f3d3933433e5956f47033ecf4b7c853dae7bfb
jamiekim828 <jamiekim828@gmail.com>
2023-01-16 12:11:19 +0100

    router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+import Product from './pages/Product';
+import Wish from './pages/Wish';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/products' element={<Product />}></Route>
+        <Route path='/wish' element={<Wish />}></Route>
+        <Route path='/cart' element={<Cart />}></Route>
+      </Routes>
     </div>
   );
 }
