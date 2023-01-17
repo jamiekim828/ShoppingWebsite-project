dff764305796582c94c3bea0bc306ff01d996c82
Ismat Mashaba <ismat.mashaba96@gmail.com>
2023-01-17 22:16:45 +0100

    footer initial

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,14 @@ import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
 import ProductDetail from './components/Products/ProductDetail';
 import WishPage from './pages/WishPage';
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import React from "react";
 
 function App() {
   return (
     <div className='App'>
+        <Navbar />
       <Routes>
         <Route path='/' element={<HomePage />}></Route>
         <Route path='/products' element={<ProductPage />}></Route>
@@ -17,6 +21,7 @@ function App() {
         <Route path='/wish' element={<WishPage />}></Route>
         <Route path='/cart' element={<CartPage />}></Route>
       </Routes>
+        <Footer/>
     </div>
   );
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-
-import Navbar from '../components/Navbar/Navbar';
 import Home from '../components/Home/Home';
-import Footer from '../components/Footer/Footer';
+
 
 export default function HomePage() {
   return (
     <div>
-      <Navbar />
+
       <Home />
-      <Footer />
     </div>
   );
 }
