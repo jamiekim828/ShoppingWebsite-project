cc193c8e6bb24050bf1f613324bd9776ddb8c8b7
Ismat Mashaba <ismat.mashaba96@gmail.com>
2023-01-17 11:21:39 +0100

    navbar complete without logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,5 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Link} from "react-router-dom";
-import logo from '../Assets/logo.png'
-
 
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
@@ -10,68 +8,54 @@ import Typography from "@mui/material/Typography";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import HomeIcon from '@mui/icons-material/Home';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-
-import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
-import {Switch} from "@mui/material";
-
-
-
+import CategoryIcon from '@mui/icons-material/Category';
 
 export default function Navbar() {
-    const [mode,setMode]=useState(false)
-const theme = createTheme({
-palette: {
-    mode: mode ? 'light' : 'dark'
-}
-})
-
   return (
       <div>
-
-          <ThemeProvider theme={theme}>
-              <Box sx={{flexGrow: 1}}>
-                      <AppBar position="static"
-                             /*style={{backgroundColor: 'pink'}}*/
-                              /*sx={{
-
-                                  bgcolor: 'transparent',
-
-                                 }}*/
+          <Box sx={{flexGrow: 1}}>
+              <AppBar position="static">
+                  <Toolbar>
+                      <Typography
+                          variant="h6"
+                          noWrap
+                          component="div"
+                          sx={{display: {xs: "none", sm: "block"}}}
                       >
-                          <Toolbar>
-                              <img src={logo} alt='logo' height='80px' width='80px'/>
-                              <Box sx={{flexGrow: 20}}/>
-                              <Box sx={{display: {xs: "none", md: "flex"}}}>
-                                  <Link  to="">
-                                      {" "}
-                                      <HomeIcon/>
-                                  </Link>
-                              </Box>
-                              <Box sx={{flexGrow: 1}}/>
-                              <Box sx={{display: {xs: "none", md: "flex"}}}>
-                                  <Link  to="/wish">
-                                      <FavoriteIcon />
+                          Logo
+                      </Typography>
+                      <Box sx={{flexGrow: 20}}/>
+                      <Box sx={{display: {xs: "none", md: "flex"}}}>
+                          <Link  to="">
+                              {" "}
+                              <HomeIcon/>
+                          </Link>
+                      </Box>
+                      <Box sx={{flexGrow: 1}}/>
+                      <Box sx={{display: {xs: "none", md: "flex"}}}>
+                          <Link  to="/products">
+                              {" "}
+                              <CategoryIcon/>
+                          </Link>
+                      </Box>
+                      <Box sx={{flexGrow: 1}}/>
+                      <Box sx={{display: {xs: "none", md: "flex"}}}>
+                          <Link  to="/wish">
+                              <FavoriteIcon />
 
-                                  </Link>
-                              </Box>
-                              <Box sx={{flexGrow: 1}}/>
-                              <Box sx={{display: {xs: "none", md: "flex"}}}>
-                                  <Link  to="/cart">
-                                      {" "}
-                                      <ShoppingCartIcon/>
-                                  </Link>
-                              </Box>
-                              <Box sx={{flexGrow: 1}}/>
-                              <Box sx={{display: {xs: "none", md: "flex"}}}>
-                                  <Switch onClick={() => setMode(!mode)}/>
-                              </Box>
+                          </Link>
+                      </Box>
+                      <Box sx={{flexGrow: 1}}/>
+                      <Box sx={{display: {xs: "none", md: "flex"}}}>
+                          <Link  to="/cart">
+                              {" "}
+                              <ShoppingCartIcon/>
+                          </Link>
+                      </Box>
 
-                          </Toolbar>
-                      </AppBar>
-                  </Box>
-              {/*</Paper>*/}
-
-          </ThemeProvider>
+                  </Toolbar>
+              </AppBar>
+          </Box>
       </div>
   )
 }
