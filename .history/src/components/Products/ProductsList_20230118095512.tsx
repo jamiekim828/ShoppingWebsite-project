1b58a23e1e8c9118a9cde6360d6e3edf9b2bf0da
mayuri2018 <mayuri.vijaya@gmail.com>
2023-01-18 11:36:44 +0100

    new

---
+++ .history/src/components/Products/ProductsList_20230118095512.tsx
@@ -0,0 +1,42 @@
+import {Box} from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
+
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import ProductsItem from "./ProductsItem";
+import { fetchProductsData } from "../../redux/thunk/product";
+import { RootState, AppDispatch } from "../../redux/store";
+
+type PropType = {
+  userInput: string;
+  
+};
+
+
+export default function ProductList({ userInput }: PropType) {
+  const productsList = useSelector(
+    (state: RootState) => state.product.productList
+  );
+  const dispatch = useDispatch<AppDispatch>();
+  useEffect(() => {
+    if (userInput === "") {
+      dispatch(fetchProductsData());
+    }
+  }, [dispatch, userInput]);
+  return (
+    <div className="productList">
+      {productsList.length === 0 && (
+        <div className="loader">
+        <Box className="loading" sx={{ ml: 10, mt: 3, width: "80%" }}>
+        Loading....
+        <CircularProgress />
+      </Box>
+      </div>
+      )}
+          {productsList.map((product) => {
+            return <ProductsItem key={product.id} product={product} />
+          })}
+    </div>
+  );
+}
---
+++ .history/src/components/Search/Search_20230118093001.tsx
@@ -0,0 +1,104 @@
+import TextField from "@mui/material/TextField";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState, AppDispatch } from "../../redux/store";
+import { actions } from "../../redux/slice/product"; 
+
+
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { fetchProductsData } from "../../redux/thunk/product";
+
+type PropType = {
+  userInput: string;
+  setUserInput: React.Dispatch<React.SetStateAction<string>>;
+  filter : string;
+  setFilter : React.Dispatch<React.SetStateAction<string>>;
+};
+
+
+export default function Search({ userInput, setUserInput, filter, setFilter }: PropType) {
+  const dispatch = useDispatch<AppDispatch>();
+  const productsList = useSelector(
+    (state: RootState) => state.product.productList
+  );
+    
+  
+
+  const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserInput(e.target.value);
+    searchHandler();
+  };
+
+  const searchHandler = () => {
+    const result = productsList.filter((product) =>
+      product.title.toLowerCase().includes(userInput.toLowerCase())
+    );
+    dispatch(actions.getProductList(result));
+  };
+  
+
+  // filter selection handle change
+  const handleChange = (event: SelectChangeEvent) => {
+    setFilter(event.target.value);
+  };
+  const filterArray = [
+    'Name',
+    'Categiry',
+    'Price',
+  ];
+
+  return (
+    <div>
+      <TextField
+        id="standard-basic"
+        label="Search product"
+        variant="standard"
+        onChange={inputHandler}
+        value={userInput}
+        InputLabelProps={{
+          style: { fontFamily: "'Nunito', sans-serif" },
+        }}
+      />
+      <div className='region-option'>
+        <FormControl sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel
+            id='demo-simple-select-helper-label'
+            sx={{ fontFamily: 'nunito' }}
+          >
+           Filter
+          </InputLabel>
+          <Select
+            labelId='demo-simple-select-helper-label'
+            id='demo-simple-select-helper'
+            value={filter}
+            label='filter'
+            onChange={handleChange}
+          >
+            <MenuItem
+              sx={{ fontFamily: 'nunito' }}
+              value=''
+              onClick={() => {
+                dispatch(fetchProductsData());
+              }}
+            >
+              <em>All</em>
+            </MenuItem>
+            {filterArray.map((filterName) => (
+              <MenuItem
+                sx={{ fontFamily: 'nunito' }}
+                onClick={() => {
+                  dispatch(fetchProductsData());
+                }}
+                key={filterName}
+              >
+                {filterName}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+    </div>
+  );
+};
