baa4a0faf360f749e9da9121d0496d5207e71b3f
mayuri2018 <mayuri.vijaya@gmail.com>
2023-01-19 08:09:10 +0100

    favcolor change

---
+++ .history/src/components/Products/ProductCard_20230118192823.tsx
@@ -0,0 +1,28 @@
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+
+import { AppDispatch, RootState } from '../../redux/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchOneProductData } from '../../redux/thunk/product';
+import ProductDetail from "./ProductDetail"
+
+export default function ProductCard() {
+  const { id } = useParams();
+  const productId = Number(id);
+  const productDetail = useSelector(
+    (state: RootState) => state.product.product
+  );
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(fetchOneProductData(productId));
+  }, [dispatch, productId]);
+
+  console.log(productDetail[0]);
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <ProductDetail productDetail={productDetail[0]} />
+    </div>
+  );
+}
